b308003974087b85cb3d5b74d6ae28b26229a17e
Chintan Acharya <acharya.chintan96@gmail.com>
2016-06-18 00:56:02 +0530

    Added player class and completed enemy ctor

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +16,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    this.x += dt * this.speed;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -25,12 +29,29 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function(x, y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/char-boy.png';
+}
 
+Player.prototype.update = function(dt) {
+    this.x += dt * 10;
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+allEnemies = [
+    new Enemy(0, 60, 25),
+    new Enemy(0, 145, 25),
+    new Enemy(0, 230, 25),
+    new Enemy(0, 315, 25)
+];
+player = new Player(100, 100);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
